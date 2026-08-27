71a6b596a1d99159886eab48f78d953e550c0bf8
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-08-27 18:00:15 -0300

    chore(release): prepare 0.1.1

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule BeamConsole.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/ccarvalho-eng/beam_console"
-  @version "0.1.0"
+  @version "0.1.1"
 
   def project do
     [
